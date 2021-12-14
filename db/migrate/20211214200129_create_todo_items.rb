813bcb348969cf46da3d1c101f4d4c7639095180
ocotopdf <erikgrueter@gmail.com>
2021-12-14 15:02:03 -0500

    scaffold TodoItem

---
+++ db/migrate/20211214200129_create_todo_items.rb
@@ -0,0 +1,9 @@
+class CreateTodoItems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :todo_items do |t|
+      t.references :todo_list, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
